df2dcd61563a66fae96f09261963074b307ee214
Dane <danielzainee04@gmail.com>
2026-05-17 03:16:06 +0800

    Why am i here level manager

--- Main/game_manager.gd
+++ Main/game_manager.gd
@@ -1,26 +1,41 @@
 extends Node2D
+# Driving game -> Programming Mini game -> Taiko Game -> 
+# Beating It Game -> Divorce Paper Game -> Memory Game
 
 var minigames = [
 	preload("res://DrivingMiniGame/Scenes/driving_mini_game.tscn"),
-	preload("res://ButtonMiniGame/button_mash_game.tscn"),
 	preload("res://ProgrammingMiniGame/BugSmashingSubMiniGame/mini_game_manager.tscn"),
+	preload("res://ButtonMiniGame/button_mash_game.tscn"),
 	preload("res://divorce_paper_minigame/divorce_paper_minigame.tscn"),
 	preload("res://memory_pick_minigame/memory_pick.tscn")
 ]
+
 var current_level
+var levelProgression = 0
+var changeLevel:bool = false
 @onready var MinigameContainer = $"../Minigames"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	load_level(0)
+	load_level(levelProgression)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+
+func _on_minigame_finished():
+	levelProgression += 1
+	
+	if levelProgression < minigames.size():
+		load_level(levelProgression)
+	else:
+		print("All minigames finished")
 
 func load_level(index):
 	if current_level:
 		current_level.queue_free()
 	current_level = minigames[index].instantiate()
+	current_level.minigame_finished.connect(_on_minigame_finished)
 	MinigameContainer.add_child(current_level)
 	
