a620f1e2c82bad6804e6c2cd1a0284d9fa867bde
k3mystra <dev@k3mystra.cc>
2026-05-16 22:24:23 +0800

    fix: Make each pen stroke separate

--- divorce_paper_minigame/document.gd
+++ divorce_paper_minigame/document.gd
@@ -1,12 +1,11 @@
 extends CharacterBody2D
 
 var sign_position: Vector2
-var sign_points: Array[Vector2]
+var sign_strokes = []
 
 enum DocState { PEN_DOWN, IDLE}
 
 var current_doc_state: DocState = DocState.IDLE
-var points
 var mouse_speed : float
 
 @onready var loop = $loop
@@ -14,23 +13,27 @@ var mouse_speed : float
 # PRELOAD SOUNDS HERE
 var down = preload("res://divorce_paper_minigame/sounds/pen_down.ogg")
 
+# Sound stuff
 func _process(delta: float) -> void:
 	if mouse_speed > 0:
 		mouse_speed = lerpf(mouse_speed, 0.0, delta * 10.0)
 		var target_pitch = remap(mouse_speed, 0, 300, 0.1, 1.5)
 		loop.pitch_scale = clampf(target_pitch, 0.1, 1.5)
 
+
 func _draw() -> void:
-	if sign_points.size() < 2:
-		return
+	print(sign_strokes.size())
+	for sign_points in sign_strokes:
+		if sign_points.size() < 2:
+			return
 
-	# Literally redraw everything every redraw call
-	# Not performant but fk it
-	for i in range(sign_points.size() - 1):
-		var curr_point = sign_points[i]
-		var next_point = sign_points[i + 1]
+		# Literally redraw everything every redraw call
+		# Not performant but fk it
+		for i in range(sign_points.size() - 1):
+			var curr_point = sign_points[i]
+			var next_point = sign_points[i + 1]
 
-		draw_line(curr_point, next_point, Color.BLACK, 4.0)
+			draw_line(curr_point, next_point, Color.BLACK, 4.0)
 
 
 func _input(event: InputEvent) -> void:
@@ -38,6 +41,7 @@ func _input(event: InputEvent) -> void:
 		if event.is_pressed():
 			current_doc_state = DocState.PEN_DOWN
 			play_sound(down, 1.5, 1)
+			add_new_stroke()
 		if event.is_released():
 			current_doc_state = DocState.IDLE
 			play_sound(down, -0.4, 1)
@@ -52,8 +56,7 @@ func _input(event: InputEvent) -> void:
 				# event.position is viewport-coordinate, need to substract our position to get local coordinate
 				# since draw_line() accepts local coords
 				loop.global_position = get_global_mouse_position()
-				sign_points.append(event.position - position)
-				# print("Drawing", sign_points[-2], sign_points[-1])
+				add_points_to_current_stroke(event.position - position)
 				queue_redraw()
 	else:
 		return
@@ -67,3 +70,11 @@ func play_sound (stream: AudioStream, pitch: float, volume: float): # YOU CAN JU
 	add_child(p) # adds to the world
 	p.play() # play first
 	p.finished.connect(p.queue_free) # remove itself after finished playing
+
+
+func add_points_to_current_stroke(pos: Vector2) -> void:
+	sign_strokes[-1].append(pos)
+
+
+func add_new_stroke() -> void:
+	sign_strokes.append([])
